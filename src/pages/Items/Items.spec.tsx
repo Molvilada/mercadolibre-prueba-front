cc13a32fa020a6713e686fc841841f90d3960c8a
Laura Molina <ldmolinav@gmail.com>
2023-01-24 21:31:33 -0500

    Arreglando test

--- src/pages/Items/Items.spec.tsx
+++ src/pages/Items/Items.spec.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Items from "./Items";
 import renderer from "react-test-renderer";
+import { state } from "../../mocks/state";
+import * as redux from "react-redux";
 
 describe("pages/Items", () => {
+  beforeEach(() => {
+    const spy = jest.spyOn(redux, "useSelector");
+    spy
+      .mockReturnValueOnce([...state.items])
+      .mockReturnValueOnce([...state.bestCategory]);
+  });
   test("Items should render Correctly", () => {
     const container = renderer.create(<Items />);
     expect(container.toJSON()).toMatchSnapshot();
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,4 @@
 import "@testing-library/jest-dom";
-import * as redux from "react-redux";
-import { state } from "./mocks/state";
 
 export const mockNavigate = jest.fn();
 
@@ -14,10 +12,3 @@ jest.mock("react-redux", () => ({
   ...jest.requireActual("react-redux"),
   useDispatch: () => jest.fn(),
 }));
-
-global.beforeEach(() => {
-  const spy = jest.spyOn(redux, "useSelector");
-  spy
-    .mockReturnValueOnce([...state.items])
-    .mockReturnValueOnce([...state.bestCategory]);
-});
